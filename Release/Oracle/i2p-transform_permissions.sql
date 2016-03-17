4dc25a7680f56340b155b6e15da4a3f586550294
Keith Wanta <kwanta@uwhealth.org>
2016-03-17 12:20:25 -0500

    MSSQL changes based on Oracle updates

--- Release/Oracle/i2p-transform_permissions.sql
+++ Release/Oracle/i2p-transform_permissions.sql
@@ -3,8 +3,8 @@
 -- ASSUMES:
 -- SCILHS\i2p-transform/Oracle/PCORI_MEDS_SCHEMA_CHANGE_ora.sql > Do not run old version of this with ALTER TABLE commands!
 
--- 'CDM' is name of schema
-alter session set current_schema=CDM;
+-- 'I2B2DEMODATA' is name of stock i2b2 CRC schema
+alter session set current_schema=I2B2DEMODATA;
 
 -- 'cdm' is name of user who needs access to 'CDM' schema
 grant all on PCORNET_DEMO to cdm;
