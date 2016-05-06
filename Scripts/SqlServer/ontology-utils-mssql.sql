64420bf51334a8225f42af11918ce7d1fbf8b252
Jeffrey Klann, PhD <jeff.klann@mgh.harvard.edu>
2016-05-06 16:35:36 -0400

    Code to fix dimcode issues in MSSQL

--- Scripts/SqlServer/ontology-utils-mssql.sql
+++ Scripts/SqlServer/ontology-utils-mssql.sql
@@ -3,7 +3,119 @@
 --    exec dbo.RunTotalNum  -- Note the totalnum scripts must be loaded also, see below..
 --    exec dbo.FixModifierDim
 --    exec dbo.FixConceptDim
+-- Other utilities required by other scripts also included here.
+-- Updated 5/6/16
 -- Jeffrey Klann, PhD
+
+
+-----------------------------------------------------------------------------------------------------------------
+-- Procedure to get a string part between two delimeters (i.e. a /)
+-- E.g., replace(m.C_FULLNAME,dbo.stringpart(m.c_fullname,'\',m.C_HLEVEL)+'\','')
+-- Jeff Klann, PhD 5/6/16
+-----------------------------------------------------------------------------------------------------------------
+drop function dbo.stringpart
+GO
+CREATE FUNCTION dbo.stringpart ( @stringToSplit VARCHAR(MAX),@delimiter char(1),@el int )
+RETURNS varchar(max) 
+AS
+BEGIN
+
+ DECLARE @name NVARCHAR(255)
+ DECLARE @pos INT
+ DECLARE @num INT
+
+ SET @num=-2
+ WHILE @num!=@el and CHARINDEX('\', @stringToSplit) > 0
+ BEGIN
+  SELECT @pos  = CHARINDEX('\', @stringToSplit)  
+  SELECT @name = SUBSTRING(@stringToSplit, 1, @pos-1)
+
+  SELECT @stringToSplit = SUBSTRING(@stringToSplit, @pos+1, LEN(@stringToSplit)-@pos)
+  SET @num=@num+1
+ END
+
+ RETURN @name
+END
+GO
+-----------------------------------------------------------------------------------------------------------------
+-- Procedure and table to strip blacklist characters from a string. Configured to remove LIKE wildcards.
+-- Adapted from Rob Garrison on Stack Overflow
+-- Jeff Klann, PhD 5/6/16
+-----------------------------------------------------------------------------------------------------------------
+-- ============================================
+-- Create a table of blacklist characters
+-- ============================================
+IF EXISTS (SELECT * FROM sys.tables WHERE [object_id] = OBJECT_ID('dbo.CharacterBlacklist'))
+  DROP TABLE dbo.CharacterBlacklist
+GO
+CREATE TABLE dbo.CharacterBlacklist (
+    CharID              int         IDENTITY,
+    DisallowedCharacter nchar(1)    NOT NULL
+)
+GO
+INSERT INTO dbo.CharacterBlacklist (DisallowedCharacter) VALUES ('%')
+INSERT INTO dbo.CharacterBlacklist (DisallowedCharacter) VALUES ('_')
+INSERT INTO dbo.CharacterBlacklist (DisallowedCharacter) VALUES ('[')
+INSERT INTO dbo.CharacterBlacklist (DisallowedCharacter) VALUES (']')
+
+GO
+IF EXISTS (SELECT * FROM sys.objects WHERE [object_id] = OBJECT_ID('dbo.StripBlacklistCharacters'))
+  DROP FUNCTION dbo.StripBlacklistCharacters
+GO
+CREATE FUNCTION dbo.StripBlacklistCharacters (
+    @String nvarchar(900)
+)
+RETURNS varchar(900)
+AS BEGIN
+  DECLARE @blacklistCt  int
+  DECLARE @ct           int
+  DECLARE @c            nchar(1)
+
+  SELECT @blacklistCt = COUNT(*) FROM dbo.CharacterBlacklist
+
+  SET @ct = 0
+  WHILE @ct < @blacklistCt BEGIN
+    SET @ct = @ct + 1
+
+    SELECT @String = REPLACE(@String, DisallowedCharacter, '')
+    FROM dbo.CharacterBlacklist
+    WHERE CharID = @ct
+  END
+
+  RETURN (@String)
+END
+GO
+
+-----------------------------------------------------------------------------------------------------------------
+-- Run arbitrary SQL on all rows in each table listed in TABLE ACCESS with a key like 'PCORI%'
+-- Jeff Klann, PhD 5/6/16
+-- @Prepend, @Postpend - arguments to construct SQL e.g. @PREPEND TABLE_NAME @POSTPEND
+-----------------------------------------------------------------------------------------------------------------
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'RunUpdatePCORI') AND type in (N'P', N'PC'))
+DROP PROCEDURE RunUpdatePCORI
+GO
+
+create procedure dbo.RunUpdatePCORI(@Prepend varchar(max),@Postpend varchar(max)) as 
+
+DECLARE @sqltext NVARCHAR(4000);
+
+declare getsql cursor local for
+select @Prepend+' '+c_table_name+' '+@Postpend
+from TABLE_ACCESS where c_visualattributes like '%A%' and c_table_cd like 'PCORI%'
+
+begin
+OPEN getsql;
+FETCH NEXT FROM getsql INTO @sqltext;
+WHILE @@FETCH_STATUS = 0
+BEGIN
+	print @sqltext
+	exec sp_executesql @sqltext
+	FETCH NEXT FROM getsql INTO @sqltext;	
+END
+
+CLOSE getsql;
+DEALLOCATE getsql;
+end
 
 -----------------------------------------------------------------------------------------------------------------
 -- Procedure to set the version of the ontology file, used by upgrade scripts
