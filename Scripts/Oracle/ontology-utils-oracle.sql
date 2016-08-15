879b7f07b56bcba5ff6077426a6d81c96d9c5a54
Matthew Joss <mjoss@partners.org>
2016-08-15 15:56:43 -0400

    Updated with lower() functions to ensure proper name matching

--- Scripts/Oracle/ontology-utils-oracle.sql
+++ Scripts/Oracle/ontology-utils-oracle.sql
@@ -9,12 +9,22 @@
 -- Note that Modifier Dimension is not finished but should be an easy project
 -- Jeffrey Klann, PhD, 8/18/15
 -----------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------
+-- Updated with lower() functions to ensure proper name matching
+-- Matthew Joss, 8/3/2016
+-----------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------
+-- INSTRUCTIONS: Change the schema name in the first line to match the schema that you are working in. 
+-- Then run this script to load the procedure into the database. Run the loaded prodeucre by running the 
+-- following command in a separate query:
+-- BEGIN PCORNET_REFRESH_CDIM; END; /
+-----------------------------------------------------------------------------------------------------------------
 
-CREATE OR REPLACE PROCEDURE "MEM61"."PCORNET_REFRESH_CDIM" is
+CREATE OR REPLACE PROCEDURE "PCORNET"."PCORNET_REFRESH_CDIM" is
 sqltext VARCHAR(2000);
 cursor getsql is
 select 'insert into concept_dimension select c_dimcode AS concept_path, c_basecode AS concept_cd, c_name AS name_char, null AS concept_blob, update_date AS update_date, download_date as download_date, import_date as import_date, sourcesystem_cd as sourcesystem_cd, 1 as upload_id from '
-||c_table_name||' where m_applied_path=''@'' and c_tablename=''CONCEPT_DIMENSION'' and c_columnname=''concept_path'' and (c_columndatatype=''T'' or c_columndatatype=''N'') and c_synonym_cd = ''N'' and m_exclusion_cd is null and c_basecode is not null'
+||c_table_name||' where m_applied_path=''@'' and lower(c_tablename)=''concept_dimension'' and lower(c_columnname)=''concept_path'' and (lower(c_columndatatype)=''t'' or lower(c_columndatatype)=''n'') and lower(c_synonym_cd) = ''n'' and m_exclusion_cd is null and c_basecode is not null'
 from TABLE_ACCESS where c_visualattributes like '%A%'
 ;
 begin 
