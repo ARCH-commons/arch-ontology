a1c17c04b4332cb856006ebc8732b807f0f7b1ec
Jeff Klann (Partners) <jeffklann@phs005355.partners.org>
2017-05-31 17:39:16 -0400

    Fixes PROC pcori_basecodes in addition to LAB

--- Scripts/SqlServer/fix_pcori_basecode.sql
+++ Scripts/SqlServer/fix_pcori_basecode.sql
@@ -38,3 +38,29 @@ GO
 
 -- Find ontology codes that are probably not getting transformed (might or might not be a problem)
 select * from pcornet_master_vw where (pcori_basecode=c_symbol or pcori_basecode is null) and sourcesystem_cd!='PCORNET_CDM'
+
+
+/*************************** PROC ***********************/
+
+-- Clear out pcori_basecode for all proc
+update l set pcori_basecode=null from pcornet_proc l where c_fullname like '\PCORI\PROCEDURE\%'
+GO
+-- Fix all std PROC nodes (assume sourcesystem_cd is Integration for custom rows!) - DOES *NOT* WORK FOR HCPCS!
+update l set l.pcori_basecode=case when charindex('(',c_symbol)>0 THEN substring(c_symbol,2,charindex(')',c_symbol)-2) ELSE NULL END from pcornet_proc l where sourcesystem_cd!='Integration' and c_fullname like '\PCORI\PROCEDURE\%'
+GO
+-- Recurse down the tree to update all child nodes
+with basecode as 
+( 
+    select c_fullname,pcori_basecode,c_hlevel from pcornet_proc where pcori_basecode is not null
+   union all
+    select l.c_fullname,basecode.pcori_basecode,l.c_hlevel from pcornet_proc l
+    inner join basecode on basecode.c_fullname=l.c_path 
+)
+update l set l.pcori_basecode=basecode.pcori_basecode from pcornet_proc l inner join basecode on basecode.c_fullname=l.c_fullname
+GO
+
+select c_name,case when charindex('(',c_symbol)>0 THEN substring(c_symbol,2,charindex(')',c_symbol)-2) ELSE NULL END,c_basecode,pcori_basecode from pcornet_proc where sourcesystem_cd!='Integration'
+
+
+
+
